c4ffc6036c99c8c826faf31da9af44021463fe0a
NguyenHongHa168 <hah27467@gmail.com>
2024-04-14 19:45:03 +0700

    create login page

---
+++ NHH_netflix/src/app/constants/config.ts
@@ -0,0 +1,2 @@
+export const LOGO_URL  ="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const BG_IMG_URL ="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg"
