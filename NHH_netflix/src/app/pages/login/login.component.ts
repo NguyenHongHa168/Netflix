ebeb5c5b1c8d230c95a96a1d38a1e2ad0b461638
NguyenHongHa168 <hah27467@gmail.com>
2024-04-15 19:55:04 +0700

    complete the login function

--- NHH_netflix/src/app/pages/login/login.component.ts
+++ NHH_netflix/src/app/pages/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import { routes } from './../../app.routes';
+import { Component, inject } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../service/login.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 logoURL= LOGO_URL;
 bgURL = BG_IMG_URL;
+
+email!: string;
+password!:string
+loginService = inject(LoginService)
+router = inject(Router)
+toasterService = inject(ToastrService)
+
+ngOnInit(): void {
+  if(this.loginService.isLoggedIn){
+    this.router.navigateByUrl("/browse");
+  }
+}
+
+onSubmit(){
+  if(!this.email || !this.password){
+    this.toasterService.error("provide email or password");
+    return; 
+  }
+  this.loginService.login(this.email, this.password);
+  this.toasterService.success("login success");
+  this.router.navigateByUrl('/browse');
+  
+}
+
+
 }
