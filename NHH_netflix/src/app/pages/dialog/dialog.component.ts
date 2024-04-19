936072e6e1ff0641b05284060f74edfbc1a17d79
NguyenHongHa168 <hah27467@gmail.com>
2024-04-19 17:16:52 +0700

    update btn more info

--- NHH_netflix/src/app/pages/dialog/dialog.component.ts
+++ NHH_netflix/src/app/pages/dialog/dialog.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
+import { MovieService } from './../../service/movie.service';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HeaderComponent } from '../../components/header/header.component';
+import { tmcbConfig } from '../../constants/config';
+import { Movie } from '../../types/movies';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,HeaderComponent],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent {
+  tmcbConfig= tmcbConfig;
+  bannerMovie!:Movie;
+
+  popularMovies :Movie[] =[];
+  topRateMovies :Movie[] =[];
+  nowPlayMovies :Movie[] =[];
+  upComingMovies :Movie[] =[];
+  movieService = inject(MovieService);
 
   constructor(public dialogRef: MatDialogRef<DialogComponent>) {
+  }
+
+  ngOnInit(): void {
+   console.log("tmcbConfig", tmcbConfig);
+   this.movieService.getPopularMovies().subscribe((result:any) => {
+    this.popularMovies = result.results;
+    this.bannerMovie = this.popularMovies[0];
+    this.movieService
+      .getMovieVideos(this.bannerMovie.id)
+      .subscribe((res: any) => {
+        this.bannerMovie.videoKey = res.results.find(
+          (x: any) => (x.site = 'YouTube')
+        ).key;
+      });
+  });
+
+  this.movieService.getTopRatedMovies().subscribe((result:any) => {
+    this.topRateMovies = result.results
+  });
+
+  this.movieService.getNowPlayMovies().subscribe((result:any) => {
+    this.nowPlayMovies = result.results
+  });
+
+  this.movieService.getUpComingMovies().subscribe((result:any) => {
+    this.upComingMovies = result.results
+  })
   }
 
   // onNoClick(): void {
