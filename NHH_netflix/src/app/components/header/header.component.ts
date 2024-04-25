cdfaa63882c1fde632afc090c2434e93b065c47c
NguyenHongHa168 <125455436+NguyenHongHa168@users.noreply.github.com>
2024-04-24 19:04:22 -0700

    responsive device(50%)

--- NHH_netflix/src/app/components/header/header.component.ts
+++ NHH_netflix/src/app/components/header/header.component.ts
@@ -2,8 +2,6 @@ import { Component, inject } from '@angular/core';
 import { LOGO_URL } from '../../constants/config';
 import { MatIconModule } from '@angular/material/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { NgModule } from '@angular/core';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { LoginService } from '../../service/login.service';
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
@@ -21,6 +19,7 @@ export class HeaderComponent {
   hiddenSearch: boolean = true;
   loginService = inject(LoginService);
   hiddenHeader: boolean = false;
+  hiddenMenuMobile:boolean = true;
   menuHeader = ['Home', "TvShows", "Movies", "New & Popular", "Browse by Language"];
   email!: string;
   password!: string;
@@ -31,11 +30,6 @@ export class HeaderComponent {
     if (this.loginService.isLoggedIn) {
       this.hiddenHeader = !this.hiddenHeader
     }
-    // else{
-    //   this.hiddenHeader = true;
-    // }
-    console.log("isLoggedIn", this.loginService.isLoggedIn);
-    console.log(" this.hiddenHeader", this.hiddenHeader);
   }
 
   Search() {
@@ -44,12 +38,19 @@ export class HeaderComponent {
   hiddenSeach() {
     this.hiddenSearch = true;
   }
-  
+
   logOut() {
     console.log(1);
     localStorage.clear();
     this.toasterService.success("logout success");
     this.router.navigateByUrl('/**');
+  }
+
+  checkClassMenu=document.getElementsByClassName('header-menu');
+  selectHeaderMenu(){
+    if(this.checkClassMenu){
+      this.hiddenMenuMobile = !this.hiddenMenuMobile
+    }
   }
 
 
